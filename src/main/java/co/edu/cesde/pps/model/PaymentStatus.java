26a00baa3ddf47c623fcc046084a46949f4ce655
Alejocr02 <alejocardona0@gmail.com>
2026-02-19 20:25:20 -0500

    Corrigiendo anotaciones de lombok en PaymentStatus

--- src/main/java/co/edu/cesde/pps/model/PaymentStatus.java
+++ src/main/java/co/edu/cesde/pps/model/PaymentStatus.java
@@ -23,31 +23,67 @@ import java.util.Objects;
 @Builder
 public class PaymentStatus {
 
-    private Long paymentStatusId;
-    private String name;
+package co.edu.cesde.pps.model;
 
-    // equals y hashCode basados en ID
+import lombok.*;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PaymentStatus that = (PaymentStatus) o;
-        return Objects.equals(paymentStatusId, that.paymentStatusId);
+import java.util.Objects;
+
+    /**
+     * Entidad PaymentStatus - Catálogo de estados posibles de un pago.
+     *
+     * Ejemplos: pending, approved, rejected, refunded
+     *
+     * Campos:
+     * - paymentStatusId: Identificador único del estado (PK)
+     * - name: Nombre único del estado (UNIQUE)
+     *
+     * Relaciones (futuro - etapa02):
+     * - 1:N con Payment (un estado puede aplicar a múltiples pagos)
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+
+    public class PaymentStatus {
+
+        private Long paymentStatusId;
+        private String name;
+
+        // Constructor vacío (requerido para JPA futuro)
+
+        // Constructor con campos obligatorios
+        public PaymentStatus(String name) {
+            this.name = name;
+        }
+
+        // Getters y Setters
+
+        // equals y hashCode basados en ID
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PaymentStatus that = (PaymentStatus) o;
+            return Objects.equals(paymentStatusId, that.paymentStatusId);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(paymentStatusId);
+        }
+
+        // toString sin navegación a objetos relacionados
+
+        @Override
+        public String toString() {
+            return "PaymentStatus{" +
+                    "paymentStatusId=" + paymentStatusId +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(paymentStatusId);
-    }
-
-    // toString personalizado sin navegación a objetos relacionados
-
-    @Override
-    public String toString() {
-        return "PaymentStatus{" +
-                "paymentStatusId=" + paymentStatusId +
-                ", name='" + name + '\'' +
-                '}';
-    }
-}
