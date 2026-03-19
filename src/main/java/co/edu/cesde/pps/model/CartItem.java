c8333306fbab1b6d8cb45fd1df3f8c2f5562b41f
JoseCardona2008 <josecardonaan12@cesde.net>
2026-03-18 19:31:43 -0500

    feat(model): add CartItem relations and unique constraint

--- src/main/java/co/edu/cesde/pps/model/CartItem.java
+++ src/main/java/co/edu/cesde/pps/model/CartItem.java
@@ -2,6 +2,7 @@ package co.edu.cesde.pps.model;
 
 import co.edu.cesde.pps.util.CalculationUtils;
 import co.edu.cesde.pps.util.ValidationUtils;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -36,7 +37,9 @@ import java.util.Objects;
  * - N:1 con Product (muchos items referencian a un producto)
  */
 @Entity
-@Table (name = "cart_items")
+@Table(name = "cart_items", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"cart_id", "product_id"})
+})
 @Getter
 @Setter
 @NoArgsConstructor
@@ -53,6 +56,7 @@ public class CartItem {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cart_id", nullable = false)
+    @JsonBackReference("cart-items")
     private Cart cart;
 
     @ManyToOne(fetch = FetchType.LAZY)
